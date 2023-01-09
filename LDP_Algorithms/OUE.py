8388dc0479aee88b543710ae321fe6fe5f470ac0
alikaratas17 <alikaratas17@ku.edu.tr>
2023-01-09 15:28:52 +0300

    Added OUE

---
+++ LDP_Algorithms/OUE.py
@@ -0,0 +1,27 @@
+from .LDP_Base import LDP_Base
+import numpy as np
+
+class OUE(LDP_Base):
+
+  def __init__(self,epsilon,max_D=50):
+    self.epsilon = epsilon
+    self.D = list(range(max_D))
+
+  def f(self,v):
+    vect = 1*(np.random.uniform(size=len(self.D))<(1/(np.exp(self.epsilon)+1)))
+    if np.random.uniform()<0.5:
+      vect[v] = 1
+    else:
+      vect[v] = 0
+    return vect
+    
+
+  def g(self, y_values:dict)->dict:
+    results = {}
+    total = np.array(y_values.values()).sum()
+    for item in self.D:
+      results[item] = 2.0*((np.exp(epsillon)+1)*y_values[item] - total)/(np.exp(epsilon)-1)
+    return results
+  def getD(self)->list:
+    return self.D
+
