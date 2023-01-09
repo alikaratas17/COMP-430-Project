cc497d79c6f844a61016a59945c3ccb097ff6877
alikaratas17 <alikaratas17@ku.edu.tr>
2023-01-09 15:17:05 +0300

    Added SimpleRAPPOR

--- LDP_Algorithms/SimpleRAPPOR.py
+++ LDP_Algorithms/SimpleRAPPOR.py
@@ -1,18 +1,22 @@
 from .LDP_Base import LDP_Base
 import numpy as np
 
-class GRR(LDP_Base):
+class SimpleRAPPOR(LDP_Base):
 
   def __init__(self,epsilon,max_D=50):
     self.epsilon = epsilon
     self.D = list(range(max_D))
-    self.p = np.exp(self.epsilon)/(np.exp(self.epsilon)+len(self.D)-1)
-    self.q = (1 - self.p)/(len(self.D)-1)
+    self.p = np.exp(self.epsilon/2)/(np.exp(self.epsilon/2)+1)
+    self.q = 1/(np.exp(self.epsilon/2)+1)
 
   def f(self,v):
-    probs = [self.q for _ in self.D]
-    probs[self.D.index(v)] = self.p
-    return np.random.choice(self.D,p=probs)
+    vect = np.array([0 for _ in self.D])
+    vect[v] = 1
+    flip = np.random.uniform(size=len(self.D))<self.q
+    flipped = (1-vect)
+    return vect * (keep) + flipped * flip
+      
+    
 
   def g(self, y_values:dict)->dict:
     results = {}
